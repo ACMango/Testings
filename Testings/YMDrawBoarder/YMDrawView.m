69b6cd1e0711a76b62793de2882f278237790337
ACMango <guoyanmingzb@163.com>
2018-07-18 17:22:03 +0800

    go on

--- Testings/YMDrawBoarder/YMDrawView.m
+++ Testings/YMDrawBoarder/YMDrawView.m
@@ -24,6 +24,7 @@
     
     if (self = [super initWithFrame:frame]) {
         _width = 3;
+        [self addSubview:self.backgroundIV];
     }
     return self;
 }
@@ -37,8 +38,113 @@
     return [touch locationInView:self];
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    CGPoint startP = [self pointWithTouches:touches];
+    if ([event allTouches].count == 1) { // 只有一个手指
+        YMDrawPath *path = [YMDrawPath paintPathWithLineWidth:_width color:nil starPoint:startP];
+        _path = path;
+        
+        CAShapeLayer *slayer = [CAShapeLayer layer];
+        slayer.path = path.CGPath;
+        slayer.backgroundColor = [UIColor clearColor].CGColor;
+        slayer.fillColor = [UIColor clearColor].CGColor;
+        slayer.lineCap = kCALineCapRound;
+        slayer.lineJoin = kCALineJoinRound;
+        slayer.strokeColor = [UIColor blackColor].CGColor;
+        slayer.lineWidth = path.lineWidth;
+        [self.layer addSublayer:slayer];
+        _slayer = slayer;
+        [[self mutableArrayValueForKey:@"canceledLines"] removeAllObjects];
+        [[self mutableArrayValueForKey:@"lines"] addObject:_slayer];
+    }
+}
+
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    // 获取移动点
+    CGPoint moveP = [self pointWithTouches:touches];
+    
+    if ([event allTouches].count > 1){
+        
+        [self.superview touchesMoved:touches withEvent:event];
+        
+    }else if ([event allTouches].count == 1) {
+        
+        [_path addLineToPoint:moveP];
+        
+        _slayer.path = _path.CGPath;
+        
+    }
+}
+
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    if ([event allTouches].count > 1){
+        [self.superview touchesMoved:touches withEvent:event];
+    }
+}
+
+/**
+ *  画线
+ */
+- (void)drawLine{
+    
+    [self.layer addSublayer:self.lines.lastObject];
+    
+}
+/**
+ *  清屏
+ */
+- (void)clearScreen
+{
+    
+    if (!self.lines.count) return ;
+    
+    [self.lines makeObjectsPerformSelector:@selector(removeFromSuperlayer)];
+    [[self mutableArrayValueForKey:@"lines"] removeAllObjects];
+    [[self mutableArrayValueForKey:@"canceledLines"] removeAllObjects];
+    
+}
+
+/**
+ *  撤销
+ */
+- (void)undo
+{
+    //当前屏幕已经清空，就不能撤销了
+    if (!self.lines.count) return;
+    [[self mutableArrayValueForKey:@"canceledLines"] addObject:self.lines.lastObject];
+    [self.lines.lastObject removeFromSuperlayer];
+    [[self mutableArrayValueForKey:@"lines"] removeLastObject];
+    
+}
+
+
+/**
+ *  恢复
+ */
+- (void)redo
+{
+    //当没有做过撤销操作，就不能恢复了
+    if (!self.canceledLines.count) return;
+    [[self mutableArrayValueForKey:@"lines"] addObject:self.canceledLines.lastObject];
+    [[self mutableArrayValueForKey:@"canceledLines"] removeLastObject];
+    [self drawLine];
+    
+}
+
 
 #pragma mark - getter
+- (UIImageView *)backgroundIV {
+    
+    if (_backgroundIV == nil) {
+        _backgroundIV = [[UIImageView alloc] initWithFrame:self.bounds];
+        _backgroundIV.userInteractionEnabled = YES;
+    }
+    return _backgroundIV;
+}
+
 - (NSMutableArray *)lines {
     
     if (_lines == nil) {
@@ -54,5 +160,6 @@
     }
     return _canceledLines;
 }
+
 
 @end
